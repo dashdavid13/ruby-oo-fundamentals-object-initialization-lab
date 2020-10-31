d3622140a08c4fe4cc350c09f7d03da3dc10c3a2
dashdavid13 <davidamedina619@gmail.com>
2020-10-31 14:19:59 -0400

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,37 @@
+
 class Dog 
-    def initialize(dogs_name,breed= "Mutt")
-        @name = dogs_name
-        @breed = breed
-        
-        
-      
-    end 
+  def initialize(name,breed= "Mutt")
+    @name = name
+    @breed = breed
+  end 
+
+  def name
+    @name
+  end 
+  
+  def breed
+    @breed
+  end 
 end 
+
+henry = Dog.new("Henry", "Pug")
+goopy = Dog.new("Goppy")
+puts henry.name
+puts henry.breed
+puts goopy.name
+puts goopy.breed 
+
+$best_dog_ever = "Byron the Moyen Poodle"
+
+class OpinionatedClass
+  def initialize(dog="Byron the Kleinpudel")
+    @dog = dog
+  end
+
+  def opinionate!
+    puts("#{@dog} is the best dog ever")
+  end
+end
+
+novak = OpinionatedClass.new("Novak the Goldendoodle")
+novak.opinionate!
--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,16 @@
 class Person
     def initialize(name)
-        @name = name 
+        @name = name
     end 
 
-
+    def name 
+        @name 
+    end 
 end 
+
+david = Person.new("David")
+rusvely = Person.new("Rusvely")
+
+
+puts david.name
+puts rusvely.name 
